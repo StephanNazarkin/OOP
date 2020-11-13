0caf2c7eb3580a3c27548a92bb39f6286ec195e4
StephanNazarkin <74413655+StephanNazarkin@users.noreply.github.com>
2020-11-13 15:31:21 +0200

    Add files via upload
    First laboratory work

---
+++ Lab_Work_1/LW_1/LW_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LW_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("x^2 + 2*x - th(x/2)\nEnter x value: ");
+            string inputString = Console.ReadLine();
+            int xValue = Convert.ToInt32(inputString);
+            double result = Math.Pow(xValue, 2) + 2 * xValue - Math.Tanh(xValue / 2);
+            if (xValue < 0)
+            {
+                Console.Write("("+ xValue +")" + "^2 + 2*" + "("+ xValue +")" + " - th(" + "(" + xValue +")" + "/2) = " + result);
+            }
+            else
+            {
+                Console.Write(xValue + "^2 + 2*" + xValue + " - th(" + xValue + "/2) = " + result);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
